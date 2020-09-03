21a9d51d09953e7e3bf0dc835446ad778a27ad1d
Kim Säppi <sappikim@gmail.com>
2020-09-03 18:36:31 +0300

    Lint set up + other similar setup stuff

---
+++ client/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
+import './index.css';
+import App from './App';
+
+const store = createStore(() => {});
+
+ReactDOM.render(
+    <Provider store={store}>
+        <React.StrictMode>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </React.StrictMode>
+    </Provider>,
+    document.getElementById('root')
+);
