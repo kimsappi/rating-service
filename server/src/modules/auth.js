73bb9a8bf50c0a3206850c665658d4564ad4f8e8
Kim Sappi <ksappi@student.hive.fi>
2020-09-16 09:22:17 +0300

    try/catch on potential errors in auth middleware

--- server/src/modules/auth.js
+++ server/src/modules/auth.js
@@ -2,19 +2,23 @@ const jwt = require('jsonwebtoken');
 const config = require('./config');
 
 const authenticationMiddleware = (req, res, next) => {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
-  if (!token)
+  try {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (!token)
+      return res.status(401).json('auth error');
+    else {
+      jwt.verify(token, config.TOKEN_SECRET, (err, user) => {
+        if (err)
+          return res.status(401).json('auth error');
+        else {
+          req.user = user;
+          next();
+        }
+      });
+    }
+  } catch(err) {
     return res.status(401).json('auth error');
-  else {
-    jwt.verify(token, config.TOKEN_SECRET, (err, user) => {
-      if (err)
-        return res.status(401).json('auth error');
-      else {
-        req.user = user;
-        next();
-      }
-    });
   }
 };
 
